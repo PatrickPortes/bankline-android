5c2147345e3a76ea683fcfac369532f4e8c2c8b3
PatrickPortes <patrickportes23@hotmail.com>
2022-05-10 17:57:48 -0300

    Finalizando as Interfaces e suas funcionalidades
    Ajustes no design das interfaces, implementando e finalizando o recycler view e seu adapter.

--- app/src/main/java/me/dio/bankline_android/ui/statement/BankStatementActivity.kt
+++ app/src/main/java/me/dio/bankline_android/ui/statement/BankStatementActivity.kt
@@ -4,14 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
 import me.dio.bankline_android.R
 import me.dio.bankline_android.databinding.ActivityBankStatementBinding
 import me.dio.bankline_android.domain.Correntista
+import me.dio.bankline_android.domain.Movimentacao
+import me.dio.bankline_android.domain.TipoMovimentacao
 import kotlin.IllegalArgumentException
 
 class BankStatementActivity : AppCompatActivity() {
 
-    companion object{
+    companion object {
         const val EXTRA_ACCOUNT_HOLDER = "me.dio.bankline_android.ui.statement.EXTRA_ACCOUNT_HOLDER"
     }
 
@@ -20,14 +23,31 @@ class BankStatementActivity : AppCompatActivity() {
     }
 
     private val accountHolder by lazy {
-        intent.getParcelableExtra<Correntista>(EXTRA_ACCOUNT_HOLDER) ?: throw IllegalArgumentException()
+        intent.getParcelableExtra<Correntista>(EXTRA_ACCOUNT_HOLDER)
+            ?: throw IllegalArgumentException()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        //Teste pelo LogCat pra ver se ta recebendo o ID do correntista de uma activity para outra
-        Log.d("TESTE", "Chegou o ID: ${accountHolder}")
+        //Teste pelo Logcat pra ver se ta recebendo o ID do correntista de uma activity para outra
+        // Log.d("TESTE", "Chegou o ID: ${accountHolder}")
+
+        binding.rvBankStatement.layoutManager = LinearLayoutManager(this)
+
+        findBankStatement()
+    }
+
+    private fun findBankStatement() {
+
+        val dataSet = ArrayList<Movimentacao>()
+        dataSet.add(Movimentacao(1, "10/05/22 17:30:00", "Descrição Bem Descrita!!!", 1000.0, TipoMovimentacao.RECEITA, 1))
+        dataSet.add(Movimentacao(1, "10/05/22 17:30:00", "Descrição Bem Descrita!!!", 400.0, TipoMovimentacao.DESPESA, 1))
+        dataSet.add(Movimentacao(1, "10/05/22 17:30:00", "Descrição Bem Descrita!!!", 2000.0, TipoMovimentacao.RECEITA, 1))
+        dataSet.add(Movimentacao(1, "10/05/22 17:30:00", "Descrição Bem Descrita!!!", 1000.0, TipoMovimentacao.DESPESA, 1))
+        dataSet.add(Movimentacao(1, "10/05/22 17:30:00", "Descrição Bem Descrita!!!", 500.0, TipoMovimentacao.RECEITA, 1))
+
+        binding.rvBankStatement.adapter = BankStatementAdapter(dataSet)
     }
 }
